9197ed86f2444685fce88cafaa5b9762580b0d9f
grazeex <grazielelanna@gmail.com>
2026-09-25 09:45:22 -0300

    Atualizei exercicio de capitalização

---
+++ Strings/capitalizar_palavra.py
@@ -0,0 +1,10 @@
+## 5.Capitalizar primeira letra de cada palavra
+#Escreva um programa que capitalize a primeira letra de cada palavra em uma frase.
+# Algoritimo:
+# - Receber uma frase do usuario
+# - Capitalizar a primeira letra de cada palavra
+# - Mostrar o resultado
+
+frase = input("Digite uma frase: ") 
+frase_formatada = frase.title()
+print(f'A frase formatada é: {frase_formatada}')
